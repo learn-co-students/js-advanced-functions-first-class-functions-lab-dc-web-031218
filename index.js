8b769acef28abf8d7aa3d720f02384196d01dbec
rpyoungdale <youngdale95@gmail.com>
2018-04-25 08:33:09 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (function (drivers) {
+  return drivers.slice(0, 2);
+});
+
+const returnLastTwoDrivers = (function (drivers) {
+  return drivers.slice(-2);
+});
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+];
+
+function createFareMultiplier(int) {
+  return function (fare) {
+    return fare*int;
+  }
+};
+
+const multiply2 = createFareMultiplier(2);
+
+function fareDoubler(fare) {
+  multiply2(fare);
+};
+
+
+
+// function fareTripler() {
+//
+// };
+//
+// function selectDifferentDrivers(arrayOfDrivers) {
+//
+// };
